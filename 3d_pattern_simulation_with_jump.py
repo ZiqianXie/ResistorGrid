5c3130cc81d7b7c4c10c8be4fc31a4d5e5652226
ZiqianXie <z.xie4@umiami.edu>
2020-11-09 17:22:59 -0600

    add printing with jump

--- 3d_pattern_simulation_with_jump.py
+++ 3d_pattern_simulation_with_jump.py
@@ -3,59 +3,54 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-n_resistor_x = 20
-n_resistor_y = 20
+n_resistor_x = 40
+n_resistor_y = 40
 n_resistor_z = 10
 nx = n_resistor_x + 1
 ny = n_resistor_y + 1
 nz = n_resistor_z + 1
 
 
-class CubeTrajectory:
+class CubeTrajectoryWithJump:
     def __init__(self, start_pos=(0, 0, 0), nx=nx,
-                  ny=ny, nz=nz, dir_long=(1, 0, 0), dir_short=(0, 1, 0)):
+                  ny=ny, nz=nz, dir_long=(1, 0, 0)):
         self.n = (nx, ny, nz)
         self.area = nx * ny
         self.vol = nx * ny * nz
         self.pos = np.array(start_pos)
         self.dir_cur = np.array(dir_long)
-        self.dir_short = np.array(dir_short)
         self.dir_long = np.array(dir_long)
+        self.dir_short = np.array([1, 1, 0]) - self.dir_long
         self.cnt = 1
-
-    @staticmethod
-    def vec(ind, val):
-        n = np.zeros(3, dtype='i')
-        n[ind] = val
-        return n
+        self.jump = False
 
     def step(self):
+        self.jump = False
+        prev_pos = self.pos.copy()
         if self.cnt == self.vol:
             return self.pos, self.pos
-        prev_pos = self.pos.copy()
-        self.pos += self.dir_cur
-        self.cnt += 1
-        layer = self.pos[2] % 2
         if self.cnt % self.area == 0:
-            self.dir_cur = np.array([0, 0, 1])
-            self.dir_long = self.vec(1 - layer, 2 * (self.pos[1 - layer] == 0) - 1)
-            self.dir_short = self.vec(layer, 2 * (self.pos[layer] == 0) - 1)
-            return tuple(self.pos), tuple(prev_pos)
-        proj = self.cnt % self.n[layer]
-        if proj == 0:
-            self.dir_cur = self.dir_short
-            self.dir_long = -1 * self.dir_long
-        elif proj == 1:
+            self.jump = True
+            self.dir_short = np.abs(self.dir_long)
+            self.dir_long[:2] = np.ones(2) - self.dir_short[:2]
             self.dir_cur = self.dir_long
+            self.pos = np.array([0, 0, self.pos[2] + 1]) - self.dir_long
+        elif self.cnt % np.array(self.n)[np.abs(self.dir_long).astype('bool')][0] == 0:
+            self.dir_cur = self.dir_short
+            self.dir_long *= -1
+        elif self.cnt % np.array(self.n)[np.abs(self.dir_long).astype('bool')][0] == 1:
+            self.dir_cur = self.dir_long
+        self.cnt += 1
+        self.pos += self.dir_cur
         return tuple(self.pos), tuple(prev_pos)
 
 
 def simulation(start, grid, trajectory, defect_pos, neighbor_con):
     r = []
     for i in range(trajectory.vol - 1):
+        print(i)
         r.append(neighbor_con(start, grid, trajectory, defect_pos))
     return r
-
 
 if __name__ == "__main__":
     r_u_x = 1  # resistance along the printed line, x axis
@@ -70,7 +65,7 @@ if __name__ == "__main__":
     r_z_def = 2  # between layer contacting resistance when defect
     start = (0, 0, 0)
     grid = ResistorNetwork(nx * ny, start)
-    traj = CubeTrajectory(start)
+    traj = CubeTrajectoryWithJump(start)
 
     def defect_pos(pos):
         return False
@@ -93,7 +88,8 @@ if __name__ == "__main__":
             r2 = [r_o_x, r_o_y]
             r3 = r_z
         d = np.argmax(np.abs(np.array(pos) - np.array(prev_pos)))
-        grid.connect(pos, prev_pos, r1[d])
+        if not traj.jump:
+            grid.connect(pos, prev_pos, r1[d])
         pos_overlap = tuple(np.array(pos) - trajectory.dir_short)
         d1 = np.argmax(np.abs(trajectory.dir_short))
         if within_boundary(pos_overlap) and pos_overlap != prev_pos:
@@ -107,6 +103,10 @@ if __name__ == "__main__":
         # print(grid.get_resistance(pos, start), pos, len(grid._unused_index))
         return grid.get_resistance(pos, start), pos
     r = simulation(start, grid, traj, defect_pos, neighbor_con)
-    plt.plot(list(map(lambda x: x[0], r)))
-
-
+    spatial_r = np.zeros((nx, ny, nz))
+    for resistance, (i, j, k) in r:
+        spatial_r[i, j, k] = resistance
+    for i in range(nz):
+        plt.figure()
+        plt.imshow(spatial_r[..., i])
+        plt.show()
